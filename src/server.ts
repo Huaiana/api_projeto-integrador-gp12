b2da75ce0b7eb1762a3951db5288464cd9d36e19
huaydarc@gmail.com <huaydarc@gmail.com>
2026-05-07 02:20:36 -0300

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up to date with 'origin/master'. # # Changes to be committed:

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+
+import { ClienteController } from './controllers/ClienteController'
+import { ProdutoController } from './controllers/ProdutoController'
+import { PedidoController } from './controllers/pedidoController'
+import { FreteController } from './controllers/fretecontroller'
+import { DescontoController } from './controllers/descontocontroller'
+import { AcompanhamentoController } from './controllers/acompanhamentocontroller'
+import { SuporteController } from './controllers/suportecontroller'
+
+
+const app = express()
+app.use(express.json())
+
+ClienteController()
+ProdutoController()
+PedidoController()  
+FreteController()
+DescontoController()
+AcompanhamentoController()
+SuporteController() 
+
+
+app.get('/', (req, res) => {
+  const accept = accepts(req)
+
+  res.send({
+    tiposAceitos: accept.types(),
+    linguagem: accept.languages(),
+    encoding: accept.encodings()
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000')
+})
